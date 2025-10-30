1b8fbcdbede84dc22473310506a0d2db4e7bcc3a
Dawit Worku <dawitworkujima@gmail.com>
2025-10-30 23:27:24 +0300

    Implement Header component with theme toggle functionality

--- components/ui/layout/header.tsx
+++ components/ui/layout/header.tsx
@@ -1,5 +1,32 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "../button";
+import { Moon, Sun } from "lucide-react";
+
 const Header = () => {
-  return <div>Header</div>;
+  const { setTheme, resolvedTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const id = requestAnimationFrame(() => setIsMounted(true));
+    return () => cancelAnimationFrame(id);
+  }, []);
+
+  if (!isMounted) return null;
+
+  const isDark = resolvedTheme === "dark";
+  return (
+    <header>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => setTheme(isDark ? "light" : "dark")}
+      >
+        {isDark ? <Sun /> : <Moon />}
+      </Button>
+    </header>
+  );
 };
 
 export default Header;
