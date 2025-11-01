2ede61eac7e5bd3dacc29ea2a4e392a330688641
Dawaman43 <dawitworkujima@gmail.com>
2025-11-01 08:15:36 +0300

    feat: implement DiscoverSection component with structured layout and headings

--- components/home/discover.tsx
+++ components/home/discover.tsx
@@ -1,5 +1,26 @@
 function DiscoverSection() {
-  return <section>dicover</section>;
+  return (
+    <section>
+      <div>
+        <h2>Discover your interest</h2>
+      </div>
+
+      <div className="flex justify-center items-center gap-4">
+        <div>
+          <h3>Top ranked</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3>New arrival</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3>Top sellers</h3>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default DiscoverSection;
