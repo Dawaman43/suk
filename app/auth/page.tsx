d652c83dc99b56ee2dc75e77229756d931844736
Dawaman43 <dawitworkujima@gmail.com>
2025-11-01 09:38:01 +0300

    feat: implement authentication page with login and register tabs

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,26 @@
+import Login from "@/components/auth/login";
+import Register from "@/components/auth/register";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+
 function AuthPage() {
-  return <div>auth page</div>;
+  return (
+    <div>
+      <Tabs>
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="register">
+          <Register />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default AuthPage;
